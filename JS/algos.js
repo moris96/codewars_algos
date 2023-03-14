faf3f8ceacbf22ec95a4c1d544ee2874490c514f
moris96 <moriskhoudari@gmail.com>
2023-03-14 15:32:01 -0400

    JS: recursion factorial

--- JS/algos.js
+++ JS/algos.js
@@ -55,3 +55,10 @@
 // }
 // console.log(hyperfact(6))
 
+
+//recursion - factorial
+// function factorial(n){
+//     if(n<=1) return 1 
+//     return n * factorial(n-1)
+// }
+// console.log(factorial(3))
