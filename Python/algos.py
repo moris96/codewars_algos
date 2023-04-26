8bae36aff600d3bc4dabf0c64f0d2faadbd77df4
moris96 <moriskhoudari@gmail.com>
2023-04-26 16:25:35 -0400

    Py: strings: remove the time

--- Python/algos.py
+++ Python/algos.py
@@ -208,4 +208,7 @@ import re
 #     return (n-1) * 2
 
 
-#
+#remove the time 
+# def shorten_to_date(long_date):
+#     return long_date.split(',')[0]
+# print(shorten_to_date("Friday May 2, 7pm")) 
