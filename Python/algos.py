e90595e65e2f216a450be81477efe536a20cf1f0
moris96 <moriskhoudari@gmail.com>
2023-06-21 13:29:00 -0400

    Py: is this a triangle

--- Python/algos.py
+++ Python/algos.py
@@ -414,3 +414,9 @@ import re
 # print(prime_factors(7))
 
 
+#is this a triangle?
+# def is_triangle(a, b, c):
+#     if(a+b > c) and (b+c > a) and (c+a > b): return True
+#     else: return False
+
+
