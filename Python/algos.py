f53751475d43877cb7fb5c92ea556b2b8b99fdc2
moris96 <moriskhoudari@gmail.com>
2023-09-19 09:52:25 -0400

    Py: find first non consecutive number

--- Python/algos.py
+++ Python/algos.py
@@ -511,3 +511,12 @@ import re
 
 
 
+#find first non consecutive number
+# def first_non_consecutive(arr):
+#     for i, j in enumerate(arr, arr[0]):
+#         if i!= j:
+#             return j 
+# print(first_non_consecutive([1,2,3,4,6,7,8]), 6)
+
+
+
